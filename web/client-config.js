4a0871a635e7e73bcb0328924644784394542d56
Kevin <kevinbrown2354@gmail.com>
2021-07-05 18:57:22 +0300

    Add TypeScript to Web side

--- web/client-config.js
+++ web/client-config.js
@@ -4,4 +4,10 @@ module.exports = {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'h9421zzw',
     token: process.env.SANITY_READ_TOKEN_h9421zzw,
   },
+  resolve: `gatsby-plugin-typescript`,
+  options: {
+    isTSX: true, // defaults to false
+    jsxPragma: `jsx`, // defaults to "React"
+    allExtensions: true, // defaults to false
+  },
 }
