5f00642eafc5a9eaa3fc296b784eb30e26dd2d1f
Kevin <kevinbrown2354@gmail.com>
2021-07-06 03:41:48 +0300

    Publishing config packages to NPM

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,7 +3,4 @@ const path = require('path')
 module.exports = {
   extends: path.join(__dirname, '../.eslintrc.js'),
   root: true,
-  rules: {
-    'import/no-unresolved': ['error', { ignore: ['.*:.*'] }],
-  },
 }
