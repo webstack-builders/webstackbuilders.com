1b3a89a31f1536727346b7df05ff5b58bdac7c35
Kevin <kevinbrown2354@gmail.com>
2021-08-21 06:24:36 +0300

    web: add skip-nav

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -4,4 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+import '@reach/skip-nav/styles.css'
+
+export const onRouteUpdate = ({ prevLocation }) => {
+  if (prevLocation !== null) {
+    const skipLink = document.querySelector(`[data-reach-skip-link]`)
+
+    if (skipLink) {
+      skipLink.focus()
+    }
+  }
+}
--- web/src/html.tsx
+++ web/src/html.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+/*
+ * If you need to insert custom HTML into the <head> or <footer> of each page on your site, you can use html.js.
+ * Customizing html.js is a workaround solution for when the use of the appropriate APIs is not available in gatsby-ssr.js.
+ * Consider using onRenderBody or onPreRenderHTML instead of the method above. As a further consideration, customizing
+ * html.js is not supported within a Gatsby Theme. Use the API methods mentioned instead.
+ */
+
+import { AllHTMLAttributes, FC, HTMLAttributes, ReactElement } from 'react'
 import 'styles/normalize.css'
 
 interface HtmlProps {
-  htmlAttributes?: React.HTMLAttributes
-  headComponents?: React.ReactElement[]
-  bodyAttributes?: React.AllHTMLAttributes
-  preBodyComponents?: React.ReactElement[]
+  htmlAttributes?: HTMLAttributes['lang']
+  headComponents?: ReactElement[]
+  bodyAttributes?: AllHTMLAttributes[]
+  preBodyComponents?: ReactElement[]
   body: string
-  postBodyComponents?: React.ReactElement[]
+  postBodyComponents?: ReactElement[]
 }
 
-const HTML: React.FC<HtmlProps> = (props) => {
+// Anything you render in the html.js component will not be made “live” in the client like other
+// components. If you want to dynamically update your <head> we recommend using React Helmet.
+
+// If you want to insert custom HTML into the footer, html.js is the preferred way of doing this.
+// If you’re writing a plugin, consider using the setPostBodyComponents prop in the Gatsby SSR API.
+const HTML: FC<HtmlProps> = (props) => {
   return (
     <html {...props.htmlAttributes}>
       <head>
