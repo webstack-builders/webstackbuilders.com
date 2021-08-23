09cf1a140e0267e12e7d7ba45d7e843bcc5fc19a
Kevin <kevinbrown2354@gmail.com>
2021-08-23 03:45:16 +0300

    studio/plugins/dashboard-widget-structure-menu/src: update to export arrow function

--- studio/plugins/dashboard-widget-structure-menu/src/props.ts
+++ studio/plugins/dashboard-widget-structure-menu/src/props.ts
@@ -1,8 +1,13 @@
+import { any } from 'prop-types'
 import { combineLatest } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { loadStructure } from './lib/structure'
 
-export function toPropsStream(props$) {
+interface Props {
+  [key: string]: any
+}
+
+export const toPropsStream = (props$: Props) => {
   const structure$ = loadStructure()
 
   return combineLatest(props$, structure$).pipe(
