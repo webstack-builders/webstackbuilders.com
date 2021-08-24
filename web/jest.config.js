4e77d464bc1740fade4413d7a4baa5d5123c1952
Kevin <kevinbrown2354@gmail.com>
2021-08-25 00:16:11 +0300

    setup jest in web side for gatsby

---
+++ web/jest.config.js
@@ -0,0 +1,33 @@
+const { compilerOptions } = require('./tsconfig.json')
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+
+const paths = pathsToModuleNameMapper(compilerOptions.paths, {
+  prefix: '<rootDir>/',
+})
+
+module.exports = {
+  // Config Jest so that all js/ts or jsx/tsx files will be transformed using a `jest-preprocess.js` file in the project root
+  transform: {
+    '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js',
+  },
+  // Tells Jest how to handle imports, especially for mocking static file imports which Jest can’t handle
+  moduleNameMapper: {
+    // Mock stylesheets using the `identity-obj-proxy` package
+    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
+    // For other static assets, use a manual mock called `file-mock.js` in the `__mocks__` directory
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+    // Use tsconfig paths from the tsconfig.json file
+    ...paths,
+  },
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  // necessary because Gatsby includes un-transpiled ES6 code in node_modules
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  // usually set by Gatsby, and some components need
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
+  // some DOM APIs such as localStorage need a valid URL
+  testURL: `http://localhost`,
+  // Add a necessary Gatsby global from a file in an array of files that will be included before all tests are run
+  setupFiles: [`<rootDir>/loadershim.js`],
+}
